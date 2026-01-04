c710249649eeb5576dc9842f7102cfdd0c66e483
initaman <amandabral2104@gmail.com>
2026-01-04 18:06:39 +0530

    feat(users-model): Add Users model code

---
+++ models/users.py
@@ -0,0 +1,31 @@
+from fastapi import BaseModel, EmailStr 
+from typing import Optional, List 
+from models import Event
+
+class User(BaseModel):
+    email: EmailStr
+    password: str 
+    events: Optional[List[Event]]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@gmal.com",
+                "password": "strongpassword",
+                "events": "[]"
+            }
+        }
+
+# Model for Creating signing users
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str 
+
+    class Config:
+      json_schema_extra = {
+        "example": {
+          "email": "fastapi@gmail.com",
+          "password": "strongpassword"
+        }
+      }
