77799166ead3ac254929c7916eaa31b73e9d43b1
initaman <amandabral2104@gmail.com>
2026-01-06 18:40:13 +0530

    fixed the port

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port="8080", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
