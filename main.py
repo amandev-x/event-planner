5efc95a4b0538ff32ed6dc633e7ac6bb7649d3e4
initaman <amandabral2104@gmail.com>
2026-01-08 19:09:25 +0530

    Add event routes in the main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.events import event_router
 import uvicorn
 
 app = FastAPI()
 
 # Register routes
 app.include_router(user_router, prefix="/user")
-
+app.include_router(event_router, prefix="/events")
 @app.get("/")
 async def home_page():
     return {"Message": "Hello from FASTAPI"}
