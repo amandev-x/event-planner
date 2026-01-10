5459662c238d1b405fc00bafd5499f5779568eb5
initaman <amandabral2104@gmail.com>
2026-01-10 19:12:41 +0530

    feat(event-models): Add create event model

--- models/events.py
+++ models/events.py
@@ -18,3 +18,22 @@ class Event(BaseModel):
           "location": "Google Meet"
         }
     }
+        
+# Event add model 
+class EventCreate(BaseModel):
+  title: str
+  image: str
+  description: str 
+  tags: List[str] 
+  location: str 
+
+  class Config:
+    json_schema_extra = {
+      "example": {
+        "title": "FastAPI Book Launch",
+        "image": "https://linktomyimage.com/image.png",
+        "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+        "tags": ["python", "fastapi", "book", "launch", "api", "development"],
+        "location": "Google Meet"
+      }
+    }
